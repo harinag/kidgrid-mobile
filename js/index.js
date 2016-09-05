08d1f69fce5e29fee9122f4ba5623e1d1d5ed589
Max GESK <max@huzm.ru>
2016-09-05 17:35:39 +0300

    Index page modified and Register page added

--- js/index.js
+++ js/index.js
@@ -33,7 +33,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-        $("#loading").css("display", "none");
-        $("#page1").css("display", "block");
+        $("#loading").addClass("hidden");
+        $("#app").removeClass("hidden");
     }
 };
